3b5d1eb41536c86bd9846c14d3ac7c37dc8e613a
Raed2001 <raed00635@gmail.com>
2021-11-25 20:45:37 +0100

    GUI Class

--- src/GUI.java
+++ src/GUI.java
@@ -6,10 +6,13 @@ public class GUI {
         Integer[] toSearch={13,19,9,5,12,8,7,4,21,2,6,11};
         Partition partition=new Partition();
         BubbleSearcher bubbleSearcher=new BubbleSearcher();
+        Quicksort quicksort=new Quicksort();
         System.out.println("Partition:");
         System.out.println( partition.partitioniere(toSearch,0,8));
         System.out.println("Bubble Search:");
   bubbleSearcher.bubleSearcher(toSearch);
+  System.out.println("Quicksort:");
+  quicksort.quickSort(toSearch,0,toSearch.length);
     }
 
 
--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,6 +1,18 @@
 public class Quicksort {
 
+Partition partition=new Partition();
 
+public Integer[] quickSort(Integer[] toSort,int p,int r ){
+
+    if (r>p){
+        int q =partition.partitioniere(toSort,0,toSort.length);
+        quickSort(toSort,p,--q);
+        quickSort(toSort,++q,r);
+    }
+
+
+    return toSort;
+    }
 
 
 
